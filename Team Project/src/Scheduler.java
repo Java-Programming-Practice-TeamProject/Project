56ea6479d8d81bcad4a5ae39da167c5ce0854ca8
kimjihye04 <64833671+kimjihye04@users.noreply.github.com>
2020-05-19 10:00:11 +0900

    Update Scheduler.java

--- Team Project/src/Scheduler.java
+++ Team Project/src/Scheduler.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class Scheduler {
-    private ArrayList<Calendar> calendar = new ArrayList<>();
+    private ArrayList<Calendar> calendars = new ArrayList<>();
 
     /**
      * make new calendar
@@ -11,20 +11,28 @@ public class Scheduler {
      * @exception NoNameEnteredException
      *      이름이 입력되지 않았을 때
      */
-    public void add(String name) {}
+    public void add_calendar(String name) {}
 
     /**
      * remove calendar
      * @param name  name of calendar to delete
      * @exception NoNameEnteredException
      *      이름이 입력되지 않았을 때
+     * @exception NotCalendarName
+     *      입력된 이름을 가진 calendar가 없을 때
      */
-    public void remove(String name) {}
+    public void remove_calendar(String name) {}
 
     /**
      * change name of calendar
      * @param old_name  old name of calendar
      * @param new_name  new name of calendar
+     * @exception NotCalendarNameWithOldName
+     *      old_name을 가진 calendar가 없을 때
+     * @exception AlreadyDefinedWithNewNameException
+     *      new_name과 같은 이름의 calendar가가 존재할 때
+     * @exception NoNameEnteredException
+     *      이름이 입력되지 않았을 때
      */
-    public void modify(String old_name, String new_name) {}
+    public void modify_calendar(String old_name, String new_name) {}
 }
