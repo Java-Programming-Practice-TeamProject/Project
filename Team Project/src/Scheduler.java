194c5756123b570fa6f5e7936ca211d344de9bf0
kimjihye04 <64833671+kimjihye04@users.noreply.github.com>
2020-05-19 14:48:56 +0900

    Update Scheduler.java

--- Team Project/src/Scheduler.java
+++ Team Project/src/Scheduler.java
@@ -18,7 +18,7 @@ public class Scheduler {
      * @param name  name of calendar to delete
      * @exception NoNameEnteredException
      *      이름이 입력되지 않았을 때
-     * @exception NotCalendarName
+     * @exception NoNameMatchException
      *      입력된 이름을 가진 calendar가 없을 때
      */
     public void remove_calendar(String name) {}
@@ -27,7 +27,7 @@ public class Scheduler {
      * change name of calendar
      * @param old_name  old name of calendar
      * @param new_name  new name of calendar
-     * @exception NotCalendarNameWithOldName
+     * @exception NoNameMatchException
      *      old_name을 가진 calendar가 없을 때
      * @exception AlreadyDefinedWithNewNameException
      *      new_name과 같은 이름의 calendar가가 존재할 때
