a9466fe9fa25b25cdba111fec938f4d8727631e5
kimjihye04 <64833671+kimjihye04@users.noreply.github.com>
2020-06-10 16:12:57 +0900

    Update WeeklyScheduleFrame.java

--- Team Project/src/window/WeeklyScheduleFrame.java
+++ Team Project/src/window/WeeklyScheduleFrame.java
@@ -1,214 +1,224 @@
-package window;
+import java.awt.EventQueue;
 
-import scheduler.*;
-import java.util.ArrayList;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
+import javax.swing.JButton;
+import javax.swing.border.CompoundBorder;
+import javax.swing.JLabel;
+import javax.swing.ScrollPaneConstants;
 import java.awt.Dimension;
-import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.FlowLayout;
 
-public class WeeklyScheduleFrame extends JFrame {
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import javax.swing.JScrollBar;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import java.awt.Component;
+import javax.swing.SwingConstants;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JComboBox;
+import java.awt.Font;
+
+public class WeeklyScheduleFrame {
+
+	private JFrame frame;
+	JPanel panel;
 
 	/**
-	 *
+	 * test
 	 */
-	private static final long serialVersionUID = 160699328481387563L;
-	private JTable table_2;
-	private JTable table_3;
+	/*
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					WeeklyScheduleFrame window = new WeeklyScheduleFrame();
+					window.ShowWeeklyFrame(2);
+					window.frame.setVisible(true);
+					showweeklyschedule(panel,"11111222223333344444555556666677777",0 ,20, 30, Color.YELLOW,1,0,2);
+					showweeklyschedule(panel,"ab",1 ,4, 5, Color.YELLOW,2,1,2);
+					showweeklyschedule(panel,"1111122222333334",1 ,4, 8, Color.MAGENTA,2,0,2);
+					showweeklyalldayschedule(panel,"Test1Test1Test1",5,Color.PINK,0);
+					showweeklyalldayschedule(panel,"Test2Test2Test2",5,Color.WHITE,1);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
 
-	public WeeklyScheduleFrame(ArrayList<ArrayList<Schedule>> schedule) {
-		getContentPane().setMinimumSize(new Dimension(600, 700));
-		setMinimumSize(new Dimension(600, 700));
-		getContentPane().setSize(new Dimension(500, 500));
-		setBounds(100, 100, 510, 500);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
-		
-		JPanel panel = new JPanel();
-		panel.setPreferredSize(new Dimension(600, 150));
-		getContentPane().add(panel);
-		
-		JPanel panel_2 = new JPanel();
-		panel_2.setPreferredSize(new Dimension(3000, 50));
-		panel_2.setSize(new Dimension(500, 100));
-		getContentPane().add(panel_2);
-		panel_2.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("\uC77C");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(35, 22, 75, 15);
-		panel_2.add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("\uC6D4");
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setBounds(112, 22, 75, 15);
-		panel_2.add(lblNewLabel_1);
-		
-		JLabel lblNewLabel_2 = new JLabel("\uD654");
-		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2.setBounds(188, 22, 75, 15);
-		panel_2.add(lblNewLabel_2);
-		
-		JLabel lblNewLabel_3 = new JLabel("\uC218");
-		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_3.setBounds(265, 22, 75, 15);
-		panel_2.add(lblNewLabel_3);
-		
-		JLabel lblNewLabel_1_1 = new JLabel("\uBAA9");
-		lblNewLabel_1_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1_1.setBounds(340, 22, 75, 15);
-		panel_2.add(lblNewLabel_1_1);
-		
-		JLabel lblNewLabel_2_1 = new JLabel("\uAE08");
-		lblNewLabel_2_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2_1.setBounds(415, 22, 75, 15);
-		panel_2.add(lblNewLabel_2_1);
-		
-		JLabel lblNewLabel_4 = new JLabel("\uD1A0");
-		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_4.setBounds(499, 22, 75, 15);
-		panel_2.add(lblNewLabel_4);
+	}
+	*/
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	public void ShowWeeklyFrame(int maxnumofallday) {
+		frame = new JFrame();
+		frame.getContentPane().setMinimumSize(new Dimension(1430, 700));
+		frame.setMinimumSize(new Dimension(1430, 700));
+		frame.getContentPane().setSize(new Dimension(500, 500));
+		frame.setBounds(100, 100, 510, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
 		
 		JPanel panel_1 = new JPanel();
-		panel_1.setPreferredSize(new Dimension(600, 500));
-		getContentPane().add(panel_1);
+		panel_1.setPreferredSize(new Dimension(1430, 500));
+		frame.getContentPane().add(panel_1);
 		panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.X_AXIS));
 		
 		JPanel panel_3 = new JPanel();
-		panel_3.setMinimumSize(new Dimension(500, 23));
+		panel_3.setMinimumSize(new Dimension(1400, 23));
 		JScrollPane scrollPane = new JScrollPane(panel_3);
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		panel_1.add(scrollPane);
 		panel_3.setLayout(new BoxLayout(panel_3, BoxLayout.X_AXIS));
 		
-		table_2 = new JTable();
-		table_2.setModel(new DefaultTableModel(
-			new Object[][] {
-				{"00:00"},
-				{"00:15"},
-				{"00:30"},
-				{"00:45"},
-				{"01:00"},
-				{"01:15"},
-				{"01:30"},
-				{"01:45"},
-				{"02:00"},
-				{"02:15"},
-				{"02:30"},
-				{"02:45"},
-				{"03:00"},
-				{"03:15"},
-				{"03:30"},
-				{"03:45"},
-				{"04:00"},
-				{"04:15"},
-				{"04:30"},
-				{"04:45"},
-				{"05:00"},
-				{"05:15"},
-				{"05:30"},
-				{"05:45"},
-				{"06:00"},
-				{"06:15"},
-				{"06:30"},
-				{"06:45"},
-				{"07:00"},
-				{"07:15"},
-				{"07:30"},
-				{"07:45"},
-				{"08:00"},
-				{"08:15"},
-				{"08:30"},
-				{"08:45"},
-				{"09:00"},
-				{"09:15"},
-				{"09:30"},
-				{"09:45"},
-				{"10:00"},
-				{"10:15"},
-				{"10:30"},
-				{"10:45"},
-				{"11:00"},
-				{"11:15"},
-				{"11:30"},
-				{"11:45"},
-				{"12:00"},
-				{"12:15"},
-				{"12:30"},
-				{"12:45"},
-				{"13:00"},
-				{"13:15"},
-				{"13:30"},
-				{"13:45"},
-				{"14:00"},
-				{"14:15"},
-				{"14:30"},
-				{"14:45"},
-				{"15:00"},
-				{"15:15"},
-				{"15:30"},
-				{"15:45"},
-				{"16:00"},
-				{"16:15"},
-				{"16:30"},
-				{"16:45"},
-				{"17:00"},
-				{"17:15"},
-				{"17:30"},
-				{"17:45"},
-				{"18:00"},
-				{"18:15"},
-				{"18:30"},
-				{"18:45"},
-				{"19:00"},
-				{"19:15"},
-				{"19:30"},
-				{"19:45"},
-				{"20:00"},
-				{"20:15"},
-				{"20:30"},
-				{"20:45"},
-				{"21:00"},
-				{"21:15"},
-				{"21:30"},
-				{"21:45"},
-				{"22:00"},
-				{"22:15"},
-				{"22:30"},
-				{"22:45"},
-				{"23:00"},
-				{"23:15"},
-				{"23:30"},
-				{"23:45"},
-			},
-			new String[] {
-				"New column"
-			}
-		));
-		table_2.getColumnModel().getColumn(0).setPreferredWidth(30);
-		panel_3.add(table_2);
+		this.panel = new JPanel();
+		JPanel panel_4 = this.panel;
+		panel_3.add(panel_4);
+		GridBagLayout gbl_panel_4 = new GridBagLayout();
+		panel_4.setLayout(gbl_panel_4);
 		
-		String[][] scheduleArray = new String[96][7];
-		for(int i = 0; i < 7; i++) {
-			for(Schedule s : schedule.get(i)) {
-				if (s instanceof FullDaySchedule) {
-					continue;
-				}
-				int start_hour = ((NormalSchedule) s).getStartTime().getHour(), start_min = ((NormalSchedule) s).getStartTime().getMinute();
-				int end_hour = ((NormalSchedule) s).getEndTime().getHour(), end_min = ((NormalSchedule) s).getEndTime().getMinute();
-				int start_x = (start_hour * 60 + start_min) / 15, end_x = (end_hour * 60 + end_min) / 15;
-				for (int j = start_x; j < end_x; j++) {
-					scheduleArray[j][i] = s.getName();
-				}
-			}
+		JPanel first_panel = new JPanel();
+		GridBagConstraints first_gridbag = new GridBagConstraints();
+		first_gridbag.gridx = 0;
+		first_gridbag.gridy = 0;
+		panel_4.add(first_panel,first_gridbag);
+		
+		JPanel[] panel_times = new JPanel[96];
+		GridBagConstraints[] times_constraints = new GridBagConstraints[96];
+		for(int i=0;i<96;i++) {
+			panel_times[i] = new JPanel();
+			times_constraints[i] = new GridBagConstraints();
+			times_constraints[i].gridx = 0;
+			times_constraints[i].gridy = i+3+maxnumofallday;
+			times_constraints[i].gridheight = 1;
+			times_constraints[i].gridwidth = 1;
+			
+			String temp = Integer.toString(i/4);
+			if(i/4<10)
+				temp = "0".concat(temp);
+			temp = temp.concat(":");
+			if(i%4==0)
+				temp = temp.concat("0");
+			temp = temp.concat(Integer.toString(i%4 *15));
+			panel_times[i].add(new JLabel(temp));
+			
+			panel_4.add(panel_times[i],times_constraints[i]);
 		}
-
-		table_3 = new JTable();
-		table_3.setModel(new DefaultTableModel(
-			scheduleArray,
-			new String[] {
-				"New column", "New column", "New column", "New column", "New column", "New column", "New column"
-			}
-		));
-		panel_3.add(table_3);
+		
+		JPanel[] panel_dates = new JPanel[8];
+		GridBagConstraints[] dates_constraints = new GridBagConstraints[8];
+		for(int i=0;i<7;i++) {
+			panel_dates[i] = new JPanel();
+			if(i%2==0)
+				panel_dates[i].setBackground(Color.yellow);
+			dates_constraints[i] = new GridBagConstraints();
+			dates_constraints[i].gridx = i*6+1;
+			dates_constraints[i].gridy = 0;
+			dates_constraints[i].gridwidth = 6;
+			dates_constraints[i].gridheight = 1;
+			
+			panel_4.add(panel_dates[i],dates_constraints[i]);
+		}
+		
+		panel_dates[0].add(new JLabel("                         Sun                         "));
+		panel_dates[1].add(new JLabel("                         Mon                         "));
+		panel_dates[2].add(new JLabel("                         Tue                         "));
+		panel_dates[3].add(new JLabel("                         Wed                         "));
+		panel_dates[4].add(new JLabel("                         Thu                         "));
+		panel_dates[5].add(new JLabel("                         Fri                         "));
+		panel_dates[6].add(new JLabel("                         Sat                         "));
+		
+		JSeparator sep = new JSeparator(JSeparator.HORIZONTAL);
+		GridBagConstraints gridsep = new GridBagConstraints();
+		gridsep.gridx = 1;
+		gridsep.gridy = 1;
+		gridsep.gridheight = 1;
+		gridsep.gridwidth = 6*7;
+		sep.setPreferredSize(new Dimension(1400,1));
+		panel_4.add(sep,gridsep);
+		
+		JSeparator sep1 = new JSeparator(JSeparator.HORIZONTAL);
+		GridBagConstraints gridsep1 = new GridBagConstraints();
+		gridsep1.gridx = 1;
+		gridsep1.gridy = 2+maxnumofallday;
+		gridsep1.gridheight = 1;
+		gridsep1.gridwidth = 6*7;
+		sep1.setPreferredSize(new Dimension(1400,1));
+		panel_4.add(sep1,gridsep1);
+	}
+	/*
+	 * panel : Gridbag panel
+	 * name : name of schedule
+	 * date : day of week : sun-0, mon-1, tue-2,..., sat-6
+	 * starttime, endtime : hour*4+minute%15
+	 * Color : color of calendar
+	 * num : number of overlapped schedule
+	 * order : order of this schedule among overlapped schedule
+	 * numofallday : maximum number of allday schedule of the week
+	 */
+	static void showweeklyschedule(JPanel panel,String name,int date ,int starttime, int endtime, Color color,int num, int order, int numofallday) {
+		JPanel paneltemp = new JPanel();
+		
+		paneltemp.setPreferredSize(new Dimension(210/num,(endtime-starttime)*30));
+		paneltemp.setBackground(color);
+		GridBagConstraints gridbagtemp = new GridBagConstraints();	
+			
+		gridbagtemp.gridy = 3+starttime+numofallday;
+		gridbagtemp.gridheight = endtime-starttime;
+		gridbagtemp.gridx = 1+date*6+order*(6/num);
+		gridbagtemp.gridwidth = 6/num;		
+		
+		if (name.length()>8&&num>2){
+			name = name.substring(0, 8)+"<br>"+name.substring(8,name.length());
+		}
+		name = "<html>"+name+"</html>";
+		JLabel temp = new JLabel(name);
+		temp.setFont(new Font("굴림",Font.PLAIN, 9));
+		paneltemp.add(temp);
+		panel.add(paneltemp,gridbagtemp);
+		
+	}
+	/*
+	 * panel : Gridbag panel
+	 * name : name of schedule
+	 * date : day of week : sun-0, mon-1, tue-2,..., sat-6
+	 * Color : color of calendar
+	 * order : order of this schedule among same day schedules
+	 */
+	static void showweeklyalldayschedule(JPanel panel,String name,int date ,Color color, int order) {
+		JPanel paneltemp = new JPanel();
+		
+		paneltemp.setPreferredSize(new Dimension(210,30));
+		paneltemp.setBackground(color);
+		GridBagConstraints gridbagtemp = new GridBagConstraints();	
+			
+		gridbagtemp.gridy = 2+order;
+		gridbagtemp.gridheight = 1;
+		gridbagtemp.gridx = 1+date*6;
+		gridbagtemp.gridwidth = 6;
+		
+		JLabel temp = new JLabel(name);
+		temp.setFont(new Font("굴림",Font.PLAIN, 9));
+		paneltemp.add(temp);
+		panel.add(paneltemp,gridbagtemp);
 	}
 }
